ad255bd5bb27199ce714568e615974e399c25d83
tanvirkabir47 <tanvirkabir47@gmail.com>
2024-05-07 23:37:14 +0600

    update

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('account/login', views.login_view, name = 'login')
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def all_product(request):
+    return render(request, 'shop-category.html')
+
 def about(request):
     return render(request, 'about.html')
 
